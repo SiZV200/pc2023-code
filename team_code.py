a50ee33f64d8e8dd3e258d532f47025d06e75509
Tim_Q <ccq0x0@outlook.com>
2023-04-18 20:03:07 +0800

    critical: Implement own team code.

--- team_code.py
+++ team_code.py
@@ -10,11 +10,15 @@
 ################################################################################
 
 from helper_code import *
-import numpy as np, os, sys
-import mne
-from sklearn.impute import SimpleImputer
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-import joblib
+import numpy as np
+import os
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import ConcatDataset, DataLoader, Dataset
+from tqdm import tqdm
+
 
 ################################################################################
 #
@@ -24,106 +28,127 @@ import joblib
 
 # Train your model.
 def train_challenge_model(data_folder, model_folder, verbose):
-    # Find data files.
-    if verbose >= 1:
-        print('Finding the Challenge data...')
+    save_dir = r'splited\train'
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    if len(os.listdir(save_dir)) == 0:
+        train_split(data_folder, save_dir)
 
-    patient_ids = find_data_folders(data_folder)
-    num_patients = len(patient_ids)
+    batch_size = 40
+    n_epoch = 50
+    train_ratio = 0.8
+    net = NeuralNetwork()
+    device = "cuda"
 
-    if num_patients==0:
-        raise FileNotFoundError('No data was provided.')
+    net = net.to(device)
 
-    # Create a folder for the model if it does not already exist.
-    os.makedirs(model_folder, exist_ok=True)
+    train_data = ICareDataset(save_dir)
+    train_amount = int(len(train_data) * train_ratio)
+    val_amount = len(train_data) - train_amount
+    train_set, valid_set = torch.utils.data.random_split(train_data, [train_amount, val_amount])
 
-    # Extract the features and labels.
-    if verbose >= 1:
-        print('Extracting features and labels from the Challenge data...')
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, drop_last=False, num_workers=4)
+    valid_loader = DataLoader(valid_set, shuffle=True, num_workers=4)
 
-    features = list()
-    outcomes = list()
-    cpcs = list()
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.0002, weight_decay=1e-5)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=n_epoch * len(train_loader))
 
-    for i in range(num_patients):
-        if verbose >= 2:
-            print('    {}/{}...'.format(i+1, num_patients))
+    train_losses = []
+    val_losses = []
+    best_loss = None
 
-        # Load data.
-        patient_id = patient_ids[i]
-        patient_metadata, recording_metadata, recording_data = load_challenge_data(data_folder, patient_id)
+    for epoch in range(n_epoch):
+        net.train()
+        loss_record = []
+        avg_loss = 0
 
-        # Extract features.
-        current_features = get_features(patient_metadata, recording_metadata, recording_data)
-        features.append(current_features)
+        train_pbar = tqdm(train_loader, position=0, leave=True)
+        train_pbar.set_description(f'Epoch [{epoch + 1}/{n_epoch}]')
+        train_pbar.set_postfix({'loss': "?", "train_loss": "?"})
 
-        # Extract labels.
-        current_outcome = get_outcome(patient_metadata)
-        outcomes.append(current_outcome)
-        current_cpc = get_cpc(patient_metadata)
-        cpcs.append(current_cpc)
+        for x, y in train_pbar:
+            optimizer.zero_grad()
+            x, y = x.to(device), y.to(device)
+            pred = net(x)
+            loss = criterion(pred, y)
+            loss.backward()
+            optimizer.step()
+            loss_record.append(loss.detach().item())
+            mean_train_loss = sum(loss_record) / len(loss_record)
+            train_pbar.set_postfix({'loss': loss.detach().item(), "train_loss": mean_train_loss})
+            avg_loss = mean_train_loss
+            # print(f'Epoch [{epoch + 1}/{n_epoch}]: Train loss: {mean_train_loss:.4f}')
+        scheduler.step()
+        train_losses.append(avg_loss)
 
-    features = np.vstack(features)
-    outcomes = np.vstack(outcomes)
-    cpcs = np.vstack(cpcs)
+        net.eval()
+        loss_record = []
+        valid_pbar = tqdm(valid_loader, position=0, leave=True)
+        valid_pbar.set_description("Validation:")
+        valid_pbar.set_postfix({"valid_loss": "?"})
+        for x, y in valid_pbar:
+            x, y = x.to(device), y.to(device)
+            with torch.no_grad():
+                pred = net(x)
+                loss = criterion(pred, y)
+            loss_record.append(loss.item())
+            mean_valid_loss = sum(loss_record) / len(loss_record)
+            valid_pbar.set_postfix({"valid_loss": mean_valid_loss})
 
-    # Train the models.
-    if verbose >= 1:
-        print('Training the Challenge models on the Challenge data...')
+        val_losses.append(mean_valid_loss)
 
-    # Define parameters for random forest classifier and regressor.
-    n_estimators   = 123  # Number of trees in the forest.
-    max_leaf_nodes = 456  # Maximum number of leaf nodes in each tree.
-    random_state   = 789  # Random state; set for reproducibility.
+        if best_loss is None:
+            best_loss = mean_valid_loss
+            create_dir_not_exists(model_folder)
+            torch.save(net.state_dict(), os.path.join(model_folder, "model.pth"))
+            print("Saving model to {0}...".format(model_folder))
+        elif mean_valid_loss < best_loss:
+            best_loss = mean_valid_loss
+            torch.save(net.state_dict(), os.path.join(model_folder, "model.pth"))
+            print("Saving model to {0}...".format(model_folder))
 
-    # Impute any missing features; use the mean value by default.
-    imputer = SimpleImputer().fit(features)
+    return train_losses, val_losses
 
-    # Train the models.
-    features = imputer.transform(features)
-    outcome_model = RandomForestClassifier(
-        n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state).fit(features, outcomes.ravel())
-    cpc_model = RandomForestRegressor(
-        n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state).fit(features, cpcs.ravel())
-
-    # Save the models.
-    save_challenge_model(model_folder, imputer, outcome_model, cpc_model)
-
-    if verbose >= 1:
-        print('Done.')
 
 # Load your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
 # arguments of this function.
 def load_challenge_models(model_folder, verbose):
-    filename = os.path.join(model_folder, 'models.sav')
-    return joblib.load(filename)
+    model = NeuralNetwork()
+    model.load_state_dict(torch.load(os.path.join(model_folder, "model.pth")))
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    return model.eval()
+
 
 # Run your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
 # arguments of this function.
 def run_challenge_models(models, data_folder, patient_id, verbose):
-    imputer = models['imputer']
-    outcome_model = models['outcome_model']
-    cpc_model = models['cpc_model']
+    signals = test_split(data_folder, patient_id=patient_id)
 
-    # Load data.
-    patient_metadata, recording_metadata, recording_data = load_challenge_data(data_folder, patient_id)
+    if len(signals) == 0:
+        outcome, outcome_probability, cpc = 1, 0.667, 4
+        return outcome, outcome_probability, cpc
 
-    # Extract features.
-    features = get_features(patient_metadata, recording_metadata, recording_data)
-    features = features.reshape(1, -1)
+    signals = np.array(signals)
+    signals = torch.FloatTensor(signals)
+    test_dataset = Dataset_test(signals)
+    test_loader = DataLoader(test_dataset, shuffle=False, batch_size=1)
+    pred = []
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # Impute missing data.
-    features = imputer.transform(features)
+    for item in test_loader:
+        with torch.no_grad():
+            pred.append(models(item.to(device))[0])
 
-    # Apply models to features.
-    outcome = outcome_model.predict(features)[0]
-    outcome_probability = outcome_model.predict_proba(features)[0, 1]
-    cpc = cpc_model.predict(features)[0]
-
-    # Ensure that the CPC score is between (or equal to) 1 and 5.
-    cpc = np.clip(cpc, 1, 5)
+    pred_mean = sum(pred) / len(pred)
+    pred_mean = [float(pred_mean[0]), float(pred_mean[1])]
+    outcome = 0 if pred_mean[0] > pred_mean[1] else 1
+    outcome_probability = pred_mean[1]
+    cpc = 1 + 1 * (1 - pred_mean[0]) if outcome == 0 else 2 + 3 * pred_mean[1]
 
     return outcome, outcome_probability, cpc
+
 
 ################################################################################
 #
@@ -131,93 +156,154 @@ def run_challenge_models(models, data_folder, patient_id, verbose):
 #
 ################################################################################
 
-# Save your trained model.
-def save_challenge_model(model_folder, imputer, outcome_model, cpc_model):
-    d = {'imputer': imputer, 'outcome_model': outcome_model, 'cpc_model': cpc_model}
-    filename = os.path.join(model_folder, 'models.sav')
-    joblib.dump(d, filename, protocol=0)
+def create_dir_not_exists(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
-# Extract features from the data.
-def get_features(patient_metadata, recording_metadata, recording_data):
-    # Extract features from the patient metadata.
-    age = get_age(patient_metadata)
-    sex = get_sex(patient_metadata)
-    rosc = get_rosc(patient_metadata)
-    ohca = get_ohca(patient_metadata)
-    vfib = get_vfib(patient_metadata)
-    ttm = get_ttm(patient_metadata)
 
-    # Use one-hot encoding for sex; add more variables
-    sex_features = np.zeros(2, dtype=int)
-    if sex == 'Female':
-        female = 1
-        male   = 0
-        other  = 0
-    elif sex == 'Male':
-        female = 0
-        male   = 1
-        other  = 0
-    else:
-        female = 0
-        male   = 0
-        other  = 1
+# Data preprocessing.
+def train_split(path, save_dir=None):
+    count = 0
+    patients = find_data_folders(path)
 
-    # Combine the patient features.
-    patient_features = np.array([age, female, male, other, rosc, ohca, vfib, ttm])
+    for item in patients:
+        patient_metadata, recording_metadata, recording_data = load_challenge_data(path, item)
+        channels = ['Fp1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'Fp2-F8', 'F8-T4', 'T4-T6', 'T6-O2', 'Fp1-F3',
+                    'F3-C3', 'C3-P3', 'P3-O1', 'Fp2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'Fz-Cz', 'Cz-Pz']
+        current_cpc = get_cpc(patient_metadata)
 
-    # Extract features from the recording data and metadata.
-    channels = ['Fp1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'Fp2-F8', 'F8-T4', 'T4-T6', 'T6-O2', 'Fp1-F3',
-                'F3-C3', 'C3-P3', 'P3-O1', 'Fp2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'Fz-Cz', 'Cz-Pz']
-    num_channels = len(channels)
-    num_recordings = len(recording_data)
+        for i in range(72):
+            signal_data, sampling_frequency, signal_channels = recording_data[i]
+            if signal_data is not None:
+                signal_data = reorder_recording_channels(signal_data, signal_channels,
+                                                         channels)
+                signal_data = [signal for signal in signal_data]
+                signal_data = np.array(signal_data).transpose((1, 0))
+                signal_data = np.array_split(signal_data, 30)
+                for j in range(len(signal_data)):
+                    np.save(os.path.join(save_dir, f"{count}_{current_cpc}_signal.npy"), signal_data[j])
+                    count += 1
+        print("Dataset {0} is done!".format(item))
+    print("All is done!")
 
-    # Compute mean and standard deviation for each channel for each recording.
-    available_signal_data = list()
-    for i in range(num_recordings):
-        signal_data, sampling_frequency, signal_channels = recording_data[i]
-        if signal_data is not None:
-            signal_data = reorder_recording_channels(signal_data, signal_channels, channels) # Reorder the channels in the signal data, as needed, for consistency across different recordings.
-            available_signal_data.append(signal_data)
 
-    if len(available_signal_data) > 0:
-        available_signal_data = np.hstack(available_signal_data)
-        signal_mean = np.nanmean(available_signal_data, axis=1)
-        signal_std  = np.nanstd(available_signal_data, axis=1)
-    else:
-        signal_mean = float('nan') * np.ones(num_channels)
-        signal_std  = float('nan') * np.ones(num_channels)
+def test_split(path, patient_id=None):
+    count = 0
+    patients = [patient_id]
+    signals = []
+    for item in patients:
+        patient_metadata, recording_metadata, recording_data = load_challenge_data(path, item)
+        channels = ['Fp1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'Fp2-F8', 'F8-T4', 'T4-T6', 'T6-O2', 'Fp1-F3',
+                    'F3-C3', 'C3-P3', 'P3-O1', 'Fp2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'Fz-Cz', 'Cz-Pz']
+        for record in recording_data:
+            signal_data, sampling_frequency, signal_channels = record
+            if signal_data is not None:
+                signal_data = reorder_recording_channels(signal_data, signal_channels,
+                                                         channels)
+                signal_data = [signal for signal in signal_data]
+                signal_data = np.array(signal_data).transpose((1, 0))
+                signal_data = np.array_split(signal_data, 30)
+                for j in range(len(signal_data)):
+                    signals.append([signal_data[j].transpose(1, 0)])
+                    count += 1
+        print("Dataset {0} is done!".format(item))
 
-    # Compute the power spectral density for the delta, theta, alpha, and beta frequency bands for each channel of the most
-    # recent recording.
-    index = None
-    for i in reversed(range(num_recordings)):
-        signal_data, sampling_frequency, signal_channels = recording_data[i]
-        if signal_data is not None:
-            index = i
-            break
+    return signals
 
-    if index is not None:
-        signal_data, sampling_frequency, signal_channels = recording_data[index]
-        signal_data = reorder_recording_channels(signal_data, signal_channels, channels) # Reorder the channels in the signal data, as needed, for consistency across different recordings.
 
-        delta_psd, _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency,  fmin=0.5,  fmax=8.0, verbose=False)
-        theta_psd, _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency,  fmin=4.0,  fmax=8.0, verbose=False)
-        alpha_psd, _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency,  fmin=8.0, fmax=12.0, verbose=False)
-        beta_psd,  _ = mne.time_frequency.psd_array_welch(signal_data, sfreq=sampling_frequency, fmin=12.0, fmax=30.0, verbose=False)
+# Dataset
+class ICareDataset(Dataset):
+    def __init__(self, path):
+        super(ICareDataset, self).__init__()
+        self.path = path
+        self.fileList = os.listdir(path)
 
-        delta_psd_mean = np.nanmean(delta_psd, axis=1)
-        theta_psd_mean = np.nanmean(theta_psd, axis=1)
-        alpha_psd_mean = np.nanmean(alpha_psd, axis=1)
-        beta_psd_mean  = np.nanmean(beta_psd,  axis=1)
+    def __len__(self):
+        return len(self.fileList)
 
-        quality_score = get_quality_scores(recording_metadata)[index]
-    else:
-        delta_psd_mean = theta_psd_mean = alpha_psd_mean = beta_psd_mean = float('nan') * np.ones(num_channels)
-        quality_score = float('nan')
+    def __getitem__(self, idx):
+        fname = self.fileList[idx]
+        fullpath = os.path.join(self.path, fname)
+        signal = np.load(fullpath)
+        cpc = float(fname.split('_')[1])
 
-    recording_features = np.hstack((signal_mean, signal_std, delta_psd_mean, theta_psd_mean, alpha_psd_mean, beta_psd_mean, quality_score))
+        outcome = [1, 0] if cpc <= 2 else [0, 1]
 
-    # Combine the features from the patient metadata and the recording data and metadata.
-    features = np.hstack((patient_features, recording_features))
+        cpc = torch.FloatTensor([cpc])
+        outcome = torch.FloatTensor(outcome)
+        signal = signal.transpose((1, 0))
+        signal = torch.FloatTensor(signal)
+        signal = signal.reshape((1, signal.shape[0], -1))
+        return signal, outcome
 
-    return features
+
+class Dataset_test(Dataset):
+    def __init__(self, signals):
+        super(Dataset_test, self).__init__()
+        self.signals = signals
+
+    def __len__(self):
+        return len(self.signals)
+
+    def __getitem__(self, item):
+        return self.signals[item]
+
+
+# Neural Network
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=2, padding=1),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.cnn2 = nn.Sequential(
+            nn.Conv2d(64, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=1, padding=1),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.cnn3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=1, padding=1),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.cnn4 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=1, padding=1),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512 * 2 * 64, 4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(4096, 2),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        # x = x.permute(0, 1, 3, 2)
+        x = self.cnn1(x)
+        x = self.cnn2(x)
+        x = self.cnn3(x)
+        x = self.cnn4(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+
+if __name__ == '__main__':
+    pass
